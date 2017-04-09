bc30c4d00a047e9a7c2940890e946386a6d2bb6f
wangyu61 <wangyu61@baidu.com>
2017-04-09 23:02:08 +0800

    add factory login

---
+++ app/views/factories/_factory.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! factory, :id, :created_at, :updated_at
+json.url factory_url(factory, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :factories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :factories do 
+    member do 
+      post :login
+    end
+  end
+
   resources :orders
 
   root to: 'orders#index'
